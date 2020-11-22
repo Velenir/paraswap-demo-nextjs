c99461df289bbbb295b436e683f7269db36cb07b
Velenir <velenir@gmail.com>
2020-11-22 16:27:54 +0300

    add PSWidget

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import { PSWidget } from 'paraswap-widget';
+
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <PSWidget
+      referrer="my_company"
+      providerUrl={'https://mainnet.infura.io/v3/' + process.env.NEXT_PUBLIC_INFURA_ID}
+      unlimitedAllowance={false}
+      bgColor={'#DDD'}
+      defaultPair={{ from: 'ETH', to: 'DAI', amount: '1' }}
+    />
   </Layout>
 )
 
